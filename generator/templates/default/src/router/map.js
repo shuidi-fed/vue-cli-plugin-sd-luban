3f48067635ea0d40ae9a722f8d1977f7d9fb0f22
jiwenbing <jiwenbing@shuidihuzhu.com>
2019-03-03 17:01:56 +0800

    优化结果

--- generator/templates/default/src/router/map.js
+++ generator/templates/default/src/router/map.js
@@ -1,35 +1,6 @@
-import name from 'router/name'
-
-// 深度搜索routes下的js文件
-const autoInjectRoutes = requireSubRoutes(require.context('./routes/', true, /\.js$/))
-
 export default [
   {
     path: '/',
-    name: name.home,
-    component: () => import('seaPublic/components/crossIndex')
-  },
-  // login
-  {
-    path: '/login',
-    name: name.login,
-    meta: {
-      title: '登录'
-    },
-    component: () => import('seaPublic/components/seaLogin')
-  },
-  // autoInjectRoutes
-  ...autoInjectRoutes,
-  // ...others
-  {
-    path: '*',
-    component: () => import('seaPublic/components/seaLogin')
-  },
+    component: () => import('src/views/page1')
+  }
 ]
-
-// get subRoutes
-function requireSubRoutes (r) {
-  const routes = []
-  r.keys().forEach(key => { Object.keys(r(key)).forEach(name => { routes.push(...r(key)[name]) }) })
-  return routes
-}
--- generator/templates/default/src/views/page1/index.vue
+++ generator/templates/default/src/views/page1/index.vue
@@ -1,12 +1,8 @@
 <template>
-  <h2>page2</h2>
+  <div>page</div>
 </template>
 
 <script>
 export default {
-
 }
-</script>
-
-<style>
-</style>
+</scritp>
