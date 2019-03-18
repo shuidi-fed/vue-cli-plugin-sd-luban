81cf1ae89d7eb15ffeb296959f30bcc942d64f8a
jiwenbing <jiwenbing@shuidihuzhu.com>
2019-03-18 12:12:02 +0800

    bugfix

--- generator/templates/bin/lib/ajax.js
+++ generator/templates/bin/lib/ajax.js
@@ -2,7 +2,7 @@ const Axios = require('axios')
 const qs = require('qs')
 
 const ajax = Axios.create({
-  baseURL:'http://127.0.0.1:3000',
+  baseURL:'https://luban.shuiditech.com',
   headers: {
     'Content-Type': 'application/x-www-form-urlencoded',
   }
