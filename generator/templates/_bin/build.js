95dc9bef6561bfeb0a3ac5f5171413385bac7bef
cyy <cuiyuanyuan@shuidihuzhu.com>
2019-11-15 16:27:31 +0800

    移出发布功能

--- generator/templates/_bin/build.js
+++
@@ -1,5 +0,0 @@
-const shell = require('shelljs')
-let projecName = require('../package.json').name
-projecName = projecName.includes('/') ? projecName.split('/')[1] : projecName
-
-shell.exec(`vue-cli-service build --target lib --name ${projecName} ./src/index.js`)
