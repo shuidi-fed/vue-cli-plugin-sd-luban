6e569188fafa64ec0fe3733d0604aa60ae081b61
cyy <cuiyuanyuan@shuidihuzhu.com>
2019-11-22 16:29:31 +0800

    update use sd-ajax

--- generator/templates/src/index.vue
+++ generator/templates/src/index.vue
@@ -1,7 +1,19 @@
 <template>
   <img :src="data.src" :style="data.style" @click="toLink"/>
 </template>
-<script>
+<script><% if (options.sdAjax) { %>
+// 文档：https://wiki.shuiditech.com/pages/viewpage.action?pageId=94110028
+import { createAxios, setAjaxDebugState } from 'sd-ajax'
+// 开启调试
+setAjaxDebugState(true)
+const API = createAxios({
+  timeout: 8000, // default 15000
+  baseURL: '//xxxx.com',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded' // default
+  }
+}, {})
+<% } %>
 export default {
   name: '<%=rootOptions.projectName%>',
   props: {
