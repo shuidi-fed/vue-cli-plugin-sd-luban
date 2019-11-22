3618b4802edd533e9a904c1f3bf989b09b3aaf8e
cyy <cuiyuanyuan@shuidihuzhu.com>
2019-11-22 16:01:08 +0800

    add luban save hooks

--- generator/templates/src/index.vue
+++ generator/templates/src/index.vue
@@ -11,6 +11,15 @@ export default {
     }
   },
   methods: {
+    /**
+     * 鲁班保存项目之前，会先调用此方法
+     * 如果返回了字符串，则弹出字符串中内容提示，并阻止后续保存动作
+     * 否则将继续保存
+     */
+    $$lubanSaveHooks () {
+      // return 'msg'
+      return ''
+    },
     toLink() {
       if (!this.data.href) return
       window.location.href = this.data.href
