f61de26df8ba82f4c1f13659b7d4a1ab593c014b
cyy <cuiyuanyuan@shuidihuzhu.com>
2019-12-31 11:32:41 +0800

    update @sd/luban-publisher version

--- generator/index.js
+++ generator/index.js
@@ -15,7 +15,7 @@ module.exports = (api, options, rootOptions) => {
     devDependencies: {
       "less": "^3.0.4",
       "less-loader": "^5.0.0",
-      "@sd/luban-publisher": "^0.1.4"
+      "@sd/luban-publisher": "^0.1.5"
     },
     main: "./src/index.js",
     license: "MIT",
